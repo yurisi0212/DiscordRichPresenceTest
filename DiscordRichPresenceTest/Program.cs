f71368a89d137393efa6930a7344a3aa3ab87090
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-17 07:39:53 +0900

    プロジェクト ファイルを追加します。

---
+++ DiscordRichPresenceTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using DiscordRPC;
+using System.Threading.Tasks;
+
+namespace DiscordRichPresenceTest {
+    class Program {
+        public async static Task Main() {
+            var client = new DiscordRpcClient("888182779664285716");
+			var presence = new RichPresence() {
+				Details = "VCT Stage 3 - MASTERS BERLIN Day 7",
+				State = "Tenz最強！",
+				Timestamps = Timestamps.Now,
+				Assets = new Assets() {
+					LargeImageKey = "valorant",
+					LargeImageText = "最高っす",
+					SmallImageKey = "yurisi"
+				}
+			};
+			client.Initialize();
+            
+			client.SetPresence(presence);
+			await Task.Delay(-1);
+		}
+    }
+}
