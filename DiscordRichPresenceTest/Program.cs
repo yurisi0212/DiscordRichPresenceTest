3393772fc68ee408977968cb91d7bceeea97d682
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-18 11:58:08 +0900

    なぜかスペースじゃなくてTabになっちゃった

--- DiscordRichPresenceTest/Program.cs
+++ DiscordRichPresenceTest/Program.cs
@@ -2,21 +2,21 @@
 using System.Threading.Tasks;
 
 namespace DiscordRichPresenceTest {
-	class Program {
-		public async static Task Main() {
-			var client = new DiscordRpcClient("888182779664285716");
-			client.Initialize();
-			client.SetPresence(new RichPresence() {
-				Details = "VCT Stage 3 - MASTERS BERLIN Day 7",
-				State = "Tenz最強！",
-				Timestamps = Timestamps.Now,
-				Assets = new Assets() {
-					LargeImageKey = "valorant",
-					LargeImageText = "最高っす",
-					SmallImageKey = "yurisi"
-				}
-			});
-			await Task.Delay(-1);
-		}
-	}
+   class Program {
+      public async static Task Main() {
+         var client = new DiscordRpcClient("888182779664285716");
+         client.Initialize();
+         client.SetPresence(new RichPresence() {
+            Details = "VCT Stage 3 - MASTERS BERLIN Day 7",
+            State = "Tenz最強！",
+            Timestamps = Timestamps.Now,
+            Assets = new Assets() {
+               LargeImageKey = "valorant",
+               LargeImageText = "最高っす",
+               SmallImageKey = "yurisi"
+            }
+         });
+         await Task.Delay(-1);
+      }
+   }
 }
