98e3caf1b4bc952f9b8c440a323cc053db727c3e
yurisi0212 <Dev.yurisi@gmail.com>
2023-05-14 01:24:34 +0900

    add buttons

--- DiscordRichPresenceTest/Program.cs
+++ DiscordRichPresenceTest/Program.cs
@@ -1,22 +1,33 @@
 ﻿using DiscordRPC;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DiscordRichPresenceTest {
-   class Program {
-      public async static Task Main() {
-         var client = new DiscordRpcClient("888182779664285716");
-         client.Initialize();
-         client.SetPresence(new RichPresence() {
-            Details = "VCT Stage 3 - MASTERS BERLIN Day 7",
-            State = "Tenz最強！",
-            Timestamps = Timestamps.Now,
-            Assets = new Assets() {
-               LargeImageKey = "valorant",
-               LargeImageText = "最高っす",
-               SmallImageKey = "yurisi"
-            }
-         });
-         await Task.Delay(-1);
-      }
-   }
+    class Program {
+        public async static Task Main() {
+            var client = new DiscordRpcClient("888182779664285716");
+            client.Initialize();
+            client.SetPresence(new RichPresence() {
+                Buttons = new Button[2  ] {
+                    new Button() {
+                        Label = "youtube",
+                        Url = "https://www.youtube.com/@VALORANTjp"
+                    },
+                    new Button() {
+                        Label = "twitch",
+                        Url = "https://www.twitch.tv/valorant_jpn"
+                    },
+                },
+                Details = "2023 VCT Pacific - League Play - Day X",
+                State = "#ZETA WIN",
+                Timestamps = Timestamps.Now,
+                Assets = new Assets() {
+                    LargeImageKey = "valorant",
+                    LargeImageText = "",
+                    SmallImageKey = "yurisi"
+                }
+            });
+            await Task.Delay(-1);
+        }
+    }
 }
